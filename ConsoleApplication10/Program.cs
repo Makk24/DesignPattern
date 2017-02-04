dad784616dff2ee436fb47d1ac9c30ddaf2826cd
1009576562@qq.com <1009576562@qq.com>
2017-02-04 09:58:52 +0800

    设计模式学习

---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    public class MainClass
+    {
+        private static void Shuffle()
+        {
+            var random = new Random();
+            var result = new List<string>();
+            string[] cardType = { "红桃", "黑桃", "方块", "梅花" };
+            string[] cardValue = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+            foreach (string type in cardType)
+            {
+                var list = cardValue.Select(value => string.Format("{0}{1}", type, value));
+                result.AddRange(list);
+            }
+            result = (from c in result orderby random.Next(0, 51) descending select c).ToList();
+            result.ForEach(str => Console.Write("{0},", str));
+        }
+
+        public static void Main(string[] args)
+        {
+            Shuffle();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication4/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Class1
+    {
+    }
+    public abstract class buildertest
+    {
+        public abstract void builderpart1();
+        public abstract void builderpart2();
+        public abstract product getproduct();
+    }
+
+    public class product
+    {
+        private List<string> _part = new List<string>();
+
+        public void add(string part)
+        {
+            _part.Add(part);
+        }
+
+        public void show()
+        {
+            Console.WriteLine("product parts");
+            foreach (var part in _part)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+
+    public class createbuilder1 : buildertest
+    {
+        private product _product = new product();
+
+        public override void builderpart1()
+        {
+            _product.add("part1");
+        }
+        public override void builderpart2()
+        {
+            _product.add("part2");
+        }
+        public override product getproduct()
+        {
+            return _product;
+        }
+    }
+
+    public class director
+    {
+        public void construct(buildertest builder)
+        {
+            builder.builderpart1();
+            builder.builderpart2();
+        }
+    }
+}
